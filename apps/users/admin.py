b293a839688b4ec2d7750e88eed858c45b59fd8c
Matias Jurado <juradomatias15@gmail.com>
2024-10-14 19:40:00 -0300

    Add users to admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import User
+
+
+class aUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("last_name", "first_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "usable_password",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
+
+    list_display = [
+        "email",
+        "last_name",
+        "first_name",
+        "is_superuser",
+        "is_staff",
+        "date_joined",
+    ]
+    search_fields = ("first_name", "last_name", "email")
+    ordering = ("last_name", "first_name", "email")
+
+
+admin.site.register(User, aUserAdmin)
