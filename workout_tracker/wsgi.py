a35e1e0a092d9284676f9755bfd5fb4fb3a42694
Matias Jurado <juradomatias15@gmail.com>
2024-10-19 13:04:12 -0300

    Add config to automatically detect the apps folder

--- workout_tracker/wsgi.py
+++ workout_tracker/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR / "apps"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workout_tracker.settings.develop")
 
